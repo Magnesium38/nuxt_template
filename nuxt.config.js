f3e7533f5fdb8c0e217bb5b499438f35cd5b2c0e
Eric Jensen <ericj@paceintl.com>
2018-05-21 15:39:02 -0500

    Add tailwind and clean up defaults.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,13 @@
+const path = require('path');
+const PurgecssPlugin = require('purgecss-webpack-plugin');
+const glob = require('glob-all');
+
+class TailwindExtractor {
+	static extract(content) {
+		return content.match(/[A-z0-9-:/]+/g) || []
+	}
+}
+
 module.exports = {
 	/*
 	** Headers of the page
@@ -16,15 +26,48 @@ module.exports = {
 	/*
 	** Customize the progress bar color
 	*/
-	loading: { color: '#3B8070' },
+	loading: { color: '#8dc63f' },
+
+	css: ['@/assets/scss/tailwind.scss'],
+
 	/*
 	** Build configuration
 	*/
 	build: {
+		extractCSS: true,
+		
+		postcss: [
+			require('tailwindcss')('./tailwind.js'),
+			require('autoprefixer'),
+		],
+		
 		/*
 		** Run ESLint on save
 		*/
 		extend (config, { isDev, isClient }) {
+			if (!isDev) {
+				config.plugins.push(
+					new PurgecssPlugin({
+						paths: glob.sync([
+							path.join(__dirname, './pages/**/*.vue'),
+							path.join(__dirname, './layouts/**/*.vue'),
+							path.join(__dirname, './components/**/*.vue'),
+						]),
+
+						extractors: [{
+							extractor: TailwindExtractor,
+							extensions: ['vue'],
+						}],
+
+						whitelist: [
+							'html',
+							'body',
+							'nuxt-progress',
+						],
+					}),
+				);
+			}
+
 			if (isDev && isClient) {
 				config.module.rules.push({
 					enforce: 'pre',
